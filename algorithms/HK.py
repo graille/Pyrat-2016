597b1590fa0b84d3992c6d2fb01ee0ac7c2fa08d
Clément Trassoudaine <intvoid@live.fr>
2016-09-26 21:13:13 +0200

    Correction du code & optimisations

--- algorithms/HK.py
+++ algorithms/HK.py
@@ -17,36 +17,35 @@ class HeldKalp:
         self.maze = maze
         self.locationList = locationList
         self.from_location = from_location
-        self.cheeseNb = len(cheeseLocList)
 
     def process(self):
         comb = self.combinaisons(self.locationList)
         n = len(self.locationList)
         #init
         cost = {}
-        #path = {}
-        for location in self.locationList
-            cost[[location]] = self.maze.distanceMetagraph[self.from_location][location]
-            #path[[location]] = self.maze.pathMetagraph[self.from_location][location]
+        for location in self.locationList :
+            cost[tuple([(location)])] = self.maze.distanceMetagraph[self.from_location][location]
         
         costmin = np.inf
-        #pathmin = ""
         costMinElement = None
-        for i in range(n): #On travaille dans un nombre de points croissants
+        for i in range(2,n+1): #On travaille dans un nombre de points croissants
             for element in comb :
                 if len(element) == i :
-                    for element2 in comb :
-                        for element3 in comb :
-                            if len(element3) = 1 and element2+element3 == element :
-                                cost2to3 = self.maze.distanceMetagraph[element2[-1]][element3[0]]
-                                #path2to3 = self.maze.pathMetagraph[element2[-1]][element3[0]]
-                                if element not in cost or cost[element] > cost[element2] + cost2to3:
-                                    cost[element] = cost[element2] + cost2to3 + cost[element3]
-                                    #path[element] = path[element2] + path2to3 + path[element3]
+                    t_element = tuple(element)
+                    for element3 in comb :
+                        if len(element3) == 1 :
+                            for element2 in comb :
+                                if element2+element3 == element :
+                                    t_element2 = tuple(element2)
+                                    cost2to3 = self.maze.distanceMetagraph[element2[-1]][element3[0]]
+                                    if t_element not in cost or cost[t_element] > cost[t_element2] + cost2to3:
+                                        cost[t_element] = cost[t_element2] + cost2to3
+                                        print(cost[t_element], element)
         
         for element in comb :
-            if len(element) == n and costmin > cost[element] :
-                costmin = cost[element]
+            t_element = tuple(element)
+            if len(element) == n and costmin > cost[t_element] :
+                costmin = cost[t_element]
                 costMinElement = element
         
         return costMinElement
@@ -55,17 +54,17 @@ class HeldKalp:
 
 
     def combinaisons(self, list):
-	n = len(list)
-	res = []
-	for i in range(1<<n):
-		ecr_bin = bin(i)
-		ajout0 = n+2 - len(ecr_bin)
-		ecr_bin = "0"*ajout0 + str(ecr_bin[2:len(ecr_bin)])
-		tab = []
-		for j in range(n):
-			if ecr_bin[j] == "1":
-				tab.append(list[j])	
-		for perm in it.permutations(tab):
-			perm_tab = [i for i in perm]
-			res.append(perm_tab)
-	return res
+        n = len(list)
+        res = []
+        for i in range(1<<n):
+            ecr_bin = bin(i)
+            ajout0 = n+2 - len(ecr_bin)
+            ecr_bin = "0"*ajout0 + str(ecr_bin[2:len(ecr_bin)])
+            tab = []
+            for j in range(n):
+                if ecr_bin[j] == "1":
+                    tab.append(list[j])	
+            for perm in it.permutations(tab):
+                perm_tab = [i for i in perm]
+                res.append(perm_tab)
+        return res
